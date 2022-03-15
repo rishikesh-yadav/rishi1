292aed35483a8264a843ea604f0ca4cea6bff66f
rishikesh-yadav <rishikeshy59btechcse2023@kccitm.edu.in>
2022-03-15 11:53:30 +0530

    q

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log("index")
-  res.render('index', { title: 'Express' });
-});
 
+router.get('/', function(req, res, next) {
+  console.log("Index")
+
+  res.render('Index', { title: 'Express' });
+});
+router.get('/', function(req, res, next) {
+  console.log("student")
+
+  res.render('student', { title: 'Express' });
+});
 module.exports = router;
