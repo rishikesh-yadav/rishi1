03dda263731470bb2607420eeacc7f22ac1efec7
rishikesh-yadav <rishikeshy59btechcse2023@kccitm.edu.in>
2022-03-15 10:45:39 +0530

    doit

---
+++ routes/student.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  console.log("user");
+  res.send("respond with a resource");
+});
+router.get("/create", function (req, res, next) {
+  console.log("user");
+  res.send("respond with a create");
+});
+module.exports = router;
