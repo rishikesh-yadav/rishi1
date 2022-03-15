b1ab22abc209058d444b7cb236afd1941f4dae54
rishikesh-yadav <rishikeshy59btechcse2023@kccitm.edu.in>
2022-03-15 10:08:06 +0530

    change

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  console.log('user')
   res.send('respond with a resource');
 });
-
+router.get('/create', function(req, res, next) {
+  console.log('user')
+  res.send('respond with a create');
+});
 module.exports = router;
